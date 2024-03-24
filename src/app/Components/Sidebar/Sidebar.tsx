5afe8959cf2ccc5ce194981ea6dc9e9427adeedf
aoumad <abderazzakoumad@gmail.com>
2024-03-24 22:48:38 +0000

    new update on the project

--- src/app/Components/Sidebar/Sidebar.tsx
+++ src/app/Components/Sidebar/Sidebar.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <nav>
+
+    </nav>
   )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
-import GlobalStyleProvider from "./providers/GlobalStyleProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <GlobalStyleProvider>
-          <Sidebar />
-          {children}
-        </GlobalStyleProvider>
+            <Sidebar />
+            {children}
         </body>
     </html>
   );
